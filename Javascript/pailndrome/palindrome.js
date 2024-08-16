57b6637448811e5cc65ee0fb96c08307c4dc16b9
heber59 <92112336+heber59@users.noreply.github.com>
2024-08-16 20:48:12 +0000

    palindrome complete going to submit in freecode..

--- Javascript/pailndrome/palindrome.js
+++ Javascript/pailndrome/palindrome.js
@@ -2,11 +2,20 @@ const regex = /[a-zA-Z]/g;
 const user_word_input = document.getElementById('userWord');
 const answer = document.getElementById('palindromeAnswer');
 const btn_check = document.getElementById('bntCheckPalindrom');
-
-function validregex() {
+let value='';
+function valid_regex() {
+/*this function deleat all speacial characters and spaces*/
     const user_word = user_word_input.value;
-    const value = user_word.match(regex)?.join('') || '';
-    answer.innerText = value;
+    value = user_word.match(regex)?.join('').toLowerCase() || '';
 }
-
-btn_check.addEventListener("click", validregex);
+function reverse_answer(){
+    reverse_value = value.split('').reverse().join('');
+}
+function compare_palindrome (){
+    valid_regex();
+    reverse_answer();
+    if(reverse_value === value){
+        answer.innerText =`La palabra ${value} es un palindromo`;
+    }else  answer.innerText =`La palabra ${value} no es un palindromo`;
+}
+btn_check.addEventListener("click", compare_palindrome);

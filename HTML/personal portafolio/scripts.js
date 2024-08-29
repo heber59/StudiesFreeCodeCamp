a9f13e00c96327438c20282e9e796adf842429c5
heber59 <hesan0308@gmail.com>
2024-08-29 16:03:31 -0500

    make styles and navbar create button for english / sanish

--- HTML/personal portafolio/scripts.js
+++ HTML/personal portafolio/scripts.js
@@ -1,10 +1,27 @@
 const copy_mail = document.getElementById('copyMail') 
-
+const select_about_me = document.getElementById('aboutMe')
+const select_proyects = document.getElementById('proyects')
+const select_cv = document.getElementById('cv')
+const select_others = document.getElementById('others')
 function copy(){
     console.log("yes");
     navigator.clipboard.writeText('hesan0308@gmail.com')
     alert('Texto copiado al portapapeles: hesan0308@gmail.com');
 };
+function display_about_me(){
 
+};
+function display_cv(){
 
+};
+function display_others(){
+
+};
+function display_proyects(){
+
+};
 copy_mail.addEventListener('click', copy);
+select_about_me.addEventListener('click', display_about_me);
+select_proyects.addEventListener('click', display_proyects);
+select_cv.addEventListener('click', display_cv);
+select_others.addEventListener('click', display_others);
